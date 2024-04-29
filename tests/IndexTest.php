0442550e4410be9f69b6a31ef456f785aef02c7b
xuzizheng <wan19950504@gmail.com>
2024-04-29 18:35:38 +0800

    ✨ feat(sdk):
    sdk

---
+++ tests/IndexTest.php
@@ -0,0 +1,17 @@
+<?php
+namespace Abner\Phptest\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class IndexTest extends TestCase
+{
+    public function testIndex()
+    {
+        echo 'hello world';
+    }
+
+    public function testIndex1()
+    {
+        echo 'hello world1';
+    }
+}
